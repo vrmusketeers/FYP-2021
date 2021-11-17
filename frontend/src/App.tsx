235e993c4ba91ef9b0d3672f2f5c42ddeafd54dc
shivkumarganesh <shivkumar.ganesh@sjsu.edu>
2021-11-17 09:36:37 +0530

    Updated the fetch call and redirects

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -71,12 +71,6 @@ const App = observer(()=>{
       (result) => {
         console.log('This is the result');
         console.log(result);
-      },
-      // Note: it's important to handle errors here
-      // instead of a catch() block so that we don't swallow
-      // exceptions from actual bugs in components.
-      (error) => {
-        console.log(error);
       }
     )
   },[]);
